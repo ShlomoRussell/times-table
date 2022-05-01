e3cd19cd349e16f1e665a5f460eccc9e40f2b139
Shlomo <baapteshuva@gmail.com>
2022-05-02 00:57:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const os = require("os");
+
+function createTimesTable(cb) {
+  const timesTable = [];
+  for (let i = 1; i <= 12; i++) {
+    for (let j = 0; j <= 12; j++) {
+      if (!j == 0) {
+        timesTable.push(i * j);
+      }
+    }
+  }
+  console.log(timesTable)
+ cb(timesTable);
+}
+
+createTimesTable(function (timesTable) {
+  const formatedTable = timesTable.reduce((p, c, i) => {
+    if ((i + 1) % 12 === 1) {
+      return p + os.EOL + c;
+    } else return `${p} ${c}`;
+  });
+  console.log(formatedTable);
+});
